9f9742b4435e7465d2a4af4a34cb48f6773f7dfe
pb-06 <kkandomail@gmail.com>
2025-09-19 10:52:52 +0200

    Add express and local backend, implement GET /teachers

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 
+var global = {
+    teachers: [
+        {name: 'Gipsz Jakab', hisClass: '9/A', favourite: false},
+        {name: 'Kolompár Dzsoki', hisClass: '18/C', favourite: true},
+    ],
+    //favouriteTeacherIdx: null,
+}
+
+// local test: GET http://localhost:3333/teachers
 app.get('/teachers', (req, res)=> {
     // TODO - implement GET /teachers
-    res.sendStatus(200);
+    res.status(200).json(global.teachers);
 })
 
 const port = 3333;
 app.listen(port, ()=>{
-    console.log('Node Express backend server starts on port ', port);
+    console.log('Node Express backend server starts on port', port);
 });
