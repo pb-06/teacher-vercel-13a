510cc99ff58e2ec4daf13a9bc5ce9c1b129bed4e
pb-06 <kkandomail@gmail.com>
2025-09-19 11:52:54 +0200

    Log serverless function

--- react-app/api/teachers.js
+++ react-app/api/teachers.js
@@ -8,8 +8,10 @@ var global = {
     //favouriteTeacherIdx: null,
 }
 
-//export global;
+//export global
 
 export default async function handler(req, res) {
+    if (req.method == 'GET') console.log('GET /teachers serverless function starts', global);
+
     res.status(200).json(global.teachers);
 }
