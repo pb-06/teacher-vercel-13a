456e614f6eb6d3c5cc8d11aafd7f834ac99ba039
pb-06 <kkandomail@gmail.com>
2025-09-19 12:40:07 +0200

    Log params

--- react-app/api/teachers.js
+++ react-app/api/teachers.js
@@ -12,6 +12,7 @@ var global = {
 
 export default async function handler(req, res) {
     if (req.method == 'GET') console.log('GET /teachers serverless function starts', global);
+    console.log('params', req.params);
 
     res.status(200).json(global.teachers);
 }
